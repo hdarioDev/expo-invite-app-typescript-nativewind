3d60148ac8b1a02abe095f12e988ba08df0b06e3
hdariodev <hdariodev@gmail.com>
2025-11-03 23:05:45 -0400

    entering layout animations

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,8 +1,13 @@
 import { Text, View, Pressable, Image, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { BlurView } from "expo-blur";
+import Animated, { FadeIn, FadeInUp, FadeOut, SlideInUp } from 'react-native-reanimated';
+
+const TIME_DURATION = 1000;
 
 export default function Welcome() {
+
+  const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
   const events = [
     {
@@ -33,30 +38,53 @@ export default function Welcome() {
 
     return (
         <View className="flex-1 items-center justify-center bg-yellow-950">
-            <Image source={ events[0].image } className="absolute w-full h-full" resizeMode="cover" />
+            <Animated.Image
+              key={ events[0].id }
+              source={ events[0].image }
+              className="absolute w-full h-full"
+              resizeMode="cover"
+              entering={ FadeIn.duration(TIME_DURATION) }
+              exiting={ FadeOut.duration(TIME_DURATION) }
+            />
             <View className="absolute w-full h-full bg-black/50" />
             <BlurView intensity={ 80 }>
-              <SafeAreaView>
-                <View className="h-3/5 w-full">
+              <SafeAreaView edges={['bottom']}>
+                <Animated.View className="h-3/5 w-full mt-20" entering={SlideInUp.duration(TIME_DURATION)}>
                   <ScrollView horizontal>
                     {
                       events.map((event) => (
-                        <View key={ event.id } className="h-full w-96 p-5">
+                        <View key={ event.id } className="h-full w-96 p-5 shadow-lg">
                           <Image source={ event.image } className="h-full w-full rounded-3xl" />
                         </View>
                       ))
                     }
                   </ScrollView>
-                </View>
+                </Animated.View>
                 <View className="flex-1 justify-center gap-4 p-4">
-                    <Text className="text-center text-2xl font-bold text-white/60">Welcome to</Text>
-                    <Text className="text-center text-5xl font-bold text-white">hDarioDev Invites</Text>
-                    <Text className="text-center text-lg font-bold text-white/60 mb-5">
+                    <Animated.Text
+                      className="text-center text-2xl font-bold text-white/60"
+                      entering={FadeInUp.duration(TIME_DURATION)}
+                    >
+                      Welcome to
+                        </Animated.Text>
+                    <Animated.Text
+                      className="text-center text-5xl font-bold text-white"
+                      entering={FadeIn.delay(TIME_DURATION)}
+                    >
+                      hDarioDev Invites
+                    </Animated.Text>
+                    <Animated.Text
+                      className="text-center text-lg font-bold text-white/60 mb-5"
+                      entering={FadeInUp.duration(TIME_DURATION)}
+                    >
                         Create beautiful invitations for your events. Anyone can receive invitations. Share them with the world.
-                    </Text>
-                    <Pressable className="items-center self-center rounded-full bg-white px-10 py-4">
+                    </Animated.Text>
+                    <AnimatedPressable
+                      className="items-center self-center rounded-full bg-white px-10 py-4"
+                      entering={FadeInUp.duration(TIME_DURATION)}
+                    >
                         <Text className="text-lg">Create an Event</Text>
-                    </Pressable>
+                    </AnimatedPressable>
                 </View>
               </SafeAreaView>
             </BlurView>
